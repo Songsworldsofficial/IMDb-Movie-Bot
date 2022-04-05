5413f716f0732cead04b6e23d6d514aff424a954
Songsworldsofficial <97032164+Songsworldsofficial@users.noreply.github.com>
2022-04-05 20:22:58 +0530

    Update script.py

--- script.py
+++ script.py
@@ -12,7 +12,7 @@ Just Add me to Your Group As Admin, Hit The Help Button For More Info..</b>"""
 <b>Here Is The Help For My Commands.</b>"""
 
     ABOUT_TXT = """<b>➥ My name: {}
-➥ Creator: <a href='https://t.me/zautekm'>Zaute Km</a>
+➥ Creator: <a href='https://t.me/INSANEX7'>insane_of_telegram </a>
 ➥ Library: <a href='https://docs.pyrogram.org/'>Pyrogram</a>
 ➥ Language: Python 𝟹
 ➥ Data Base: <a href='https://www.mongodb.com/'>MongoDB</a>
@@ -27,7 +27,7 @@ Source: <a href='https://github.com/josprojects/tgmoviebot'>GitHub - Click here 
 - <a href='https://t.me/josprojects'>Jos Projects</a>
 
 <b>SUPPORT GROUP</b>
-- <a href='https://t.me/+y53tWFUw6Q43NzE9'>Jos Movie Club</a>"""
+- <a href='https://t.me/INSANEX7'>insane_of_telegram </a>"""
 
     MANUALFILTER_TXT = """Help: <b>Filters</b>
 
